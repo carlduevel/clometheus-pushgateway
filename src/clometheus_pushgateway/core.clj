e93999d6754968ee9f681ef3c70095a6daef0dd0
Carl Düvel <c.a.duevel@gmail.com>
2021-03-26 22:06:43 +0100

    wip

--- src/clometheus_pushgateway/core.clj
+++ src/clometheus_pushgateway/core.clj
@@ -1,6 +1,7 @@
 (ns clometheus-pushgateway.core
   (:require [clojure.tools.logging :as log]
             [clj-http.client :as http]
+            [clometheus.core :as c]
             [clometheus.txt-format :as txt]
             [overtone.at-at :as at])
   (:import (java.net URLEncoder)
@@ -9,10 +10,10 @@
 (defn- pushgateway-url [host-name job-name]
   (format "https://%s/metrics/job/%s" host-name (URLEncoder/encode job-name "UTF-8")))
 
-(defn- push [host-name job-name]
+(defn- push [registry host-name job-name]
   (http/post (pushgateway-url host-name job-name)
              {:throw-entire-message true
-              :body                 (txt/txt-format)
+              :body                 (txt/txt-format registry)
               :content-type         "text/plain; version=0.0.4; charset=utf-8"
               :socket-timeout       10000
               :connection-timeout   10000}))
@@ -22,9 +23,11 @@
        (catch Exception e
          (log/error e "Exception caught in scheduled job."))))
 
-(defn start-pushgateway-reporter [host-name job-name & {interval-ms :interval-ms :or {interval-ms 15000}}]
-  (let [push (partial push host-name job-name)]
+(defn start-pushgateway-reporter! [host-name job-name &
+                                  {interval-ms :interval-ms registry :registry :or {interval-ms 15000 registry c/default-registry}}]
+  (let [push (partial push registry host-name job-name)]
     (push)
+    ;; TODO return job
     (at/every  interval-ms (partial exception-to-log push) (at/mk-pool)
                :initial-delay interval-ms :desc "Push Gateway")
     (.addShutdownHook (Runtime/getRuntime) (Thread. ^Runnable (partial exception-to-log push)))
--- test/clometheus_pushgateway/core_test.clj
+++ test/clometheus_pushgateway/core_test.clj
@@ -1,7 +1,17 @@
 (ns clometheus-pushgateway.core-test
-  (:require [clojure.test :refer :all]
-            [clometheus-pushgateway.core :refer :all]))
+  (:require [clojure.test :refer [deftest is]]
+            [clometheus-pushgateway.core :as sut]
+            [clj-http.client :as http]
+            [overtone.at-at :as at]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest start-pushgateway-reporter
+  (let [expected '("https://my-host/metrics/job/my-job"
+                   {:throw-entire-message true
+                    :body                 ""
+                    :content-type         "text/plain; version=0.0.4; charset=utf-8"
+                    :socket-timeout       10000 :connection-timeout 10000})]
+    (with-redefs
+      [http/post (fn[& args] (is (= args expected)) )
+       at/every  (constantly nil )]
+      (sut/start-pushgateway-reporter! "my-host" "my-job"))))
+
