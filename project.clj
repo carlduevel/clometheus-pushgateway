0dec1d4ca077e50aa1dbe04def94321b27d9f05c
Carl Düvel <c.a.duevel@gmail.com>
2021-04-02 16:41:28 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Apache License 2.0"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [online.duevel/clometheus "0.2.0"]
+                 [online.duevel/clometheus "0.2.2"]
                  [org.clojure/tools.logging "1.1.0"]
                  [overtone/at-at "1.2.0"]
-                 [clj-http "3.10.0"]]
+                 [clj-http "3.12.1"]]
   :lein-release {:deploy-via :clojars}
   :profiles {:dev {:plugins [[lein-release/lein-release "1.0.9"]]}})
