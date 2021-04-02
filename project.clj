697495341eb3f7cfb722d07dbaec18f3a8f8ab81
Carl Düvel <c.a.duevel@gmail.com>
2021-04-02 16:21:46 +0200

    lein-release plugin: bumped version from 0.2.0 to 0.2.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject online.duevel/clometheus-pushgateway "0.2.0"
+(defproject online.duevel/clometheus-pushgateway "0.2.1-SNAPSHOT"
   :description "Pushgateway library for clometheus"
   :url "https://github.com/carlduevel/clometheus-pushgateway"
   :license {:name "Apache License 2.0"
