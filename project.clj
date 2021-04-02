e07bd4e682de3c1aba1c3773957d3790b47aa75a
Carl Düvel <c.a.duevel@gmail.com>
2021-04-02 16:20:52 +0200

    Add domain

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clometheus-pushgateway "0.2.0-SNAPSHOT"
+(defproject online.duevel/clometheus-pushgateway "0.2.0-SNAPSHOT"
   :description "Pushgateway library for clometheus"
   :url "https://github.com/carlduevel/clometheus-pushgateway"
   :license {:name "Apache License 2.0"
